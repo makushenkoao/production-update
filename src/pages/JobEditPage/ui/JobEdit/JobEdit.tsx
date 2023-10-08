18fc2efd306095f232b8f89a38501f2372c05f89
Anton Makushenko <antonmakushenko@iMac-Anton.local>
2023-10-08 16:40:51 +0300

    add edit vacancy

--- src/pages/JobEditPage/ui/JobEdit/JobEdit.tsx
+++ src/pages/JobEditPage/ui/JobEdit/JobEdit.tsx
@@ -1,14 +1,68 @@
-import { memo } from 'react';
+import { FormEvent, memo, useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { getJobDetailsData, getJobDetailsIsLoading } from '@/entities/Job';
+import { useNavigate } from 'react-router-dom';
+import {
+    FieldName,
+    getJobDetailsData,
+    Job,
+    updateJobService,
+} from '@/entities/Job';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import { Text } from '@/shared/ui/redesigned/Text';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { JobCategory } from '@/shared/const/job';
+import { getRouteJobDetails } from '@/shared/const/router';
+import { JobFields } from '../JobFields/JobFields';
 
 export const JobEdit = memo(() => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
     const job = useSelector(getJobDetailsData);
-    const loading = useSelector(getJobDetailsIsLoading);
+
+    const [formData, setFormData] = useState<Job>({
+        ...(job || ({} as Job)),
+        user: undefined,
+    });
+
+    const handleInputChange = useCallback(
+        (v: string, name: FieldName) => {
+            setFormData({
+                ...formData,
+                [name]: v,
+            });
+        },
+        [formData],
+    );
+
+    const handleChangeCategory = useCallback((c: JobCategory) => {
+        setFormData((prev) => {
+            if (prev.category.includes(c)) {
+                return {
+                    ...prev,
+                    category: prev.category.filter((p) => p !== c),
+                };
+            }
+            return {
+                ...prev,
+                category: [...prev.category, c],
+            };
+        });
+    }, []);
+
+    const onSubmit = useCallback(
+        (e: FormEvent<HTMLFormElement>) => {
+            setLoading(true);
+            e.preventDefault();
+            dispatch(updateJobService(formData)).finally(() => {
+                setLoading(false);
+                navigate(getRouteJobDetails(formData.id));
+            });
+        },
+        [dispatch, formData, navigate],
+    );
 
     return (
         <VStack
@@ -16,6 +70,13 @@ export const JobEdit = memo(() => {
             gap="16"
         >
             <Text title={t('Редагування вакансії')} />
+            <JobFields
+                formData={formData}
+                handleInputChange={handleInputChange}
+                handleChangeCategory={handleChangeCategory}
+                onSubmit={onSubmit}
+                loading={loading}
+            />
         </VStack>
     );
 });
